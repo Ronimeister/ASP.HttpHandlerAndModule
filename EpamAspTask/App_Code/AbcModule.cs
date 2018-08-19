1424a04dd72f45b1abcf473f5ccb01b6af319836
Ronimeister <ronimeisterhs@gmail.com>
2018-08-19 19:51:15 +0300

    Project added

---
+++ EpamAspTask/App_Code/AbcModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for AbcModule
+/// </summary>
+public class AbcModule : IHttpModule
+{
+    public AbcModule()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public void Dispose() { }
+
+    public void Init(HttpApplication context)
+    {
+        context.BeginRequest +=
+            (new EventHandler(Application_BeginRequest));
+        context.EndRequest +=
+            (new EventHandler(Application_EndRequest));
+    }
+
+    private void Application_BeginRequest(Object source, EventArgs e)
+    {
+        HttpApplication application = (HttpApplication)source;
+        HttpContext context = application.Context;
+        string filePath = context.Request.FilePath;
+        string fileExtension =
+            VirtualPathUtility.GetExtension(filePath);
+        if (fileExtension.Equals(".abc"))
+        {
+            context.Response.Write($"HelloWorldModule: Beginning of Request - {DateTime.Now}<hr>");
+        }
+    }
+
+    private void Application_EndRequest(Object source, EventArgs e)
+    {
+        HttpApplication application = (HttpApplication)source;
+        HttpContext context = application.Context;
+        string filePath = context.Request.FilePath;
+        string fileExtension =
+            VirtualPathUtility.GetExtension(filePath);
+        if (fileExtension.Equals(".abc"))
+        {
+            context.Response.Write($"<hr>HelloWorldModule: End of Request - {DateTime.Now}");
+        }
+    }
+}
---
+++ EpamAspTask/App_Code/SimpleHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SimpleHandler
+/// </summary>
+public class SimpleHandler : IHttpHandler
+{
+    public SimpleHandler()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public bool IsReusable
+    {
+        get { return true; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+        HttpRequest request = context.Request;
+        HttpResponse response = context.Response;
+
+        foreach (string i in request.Headers.Keys)
+        {
+            response.Write($"<p><b>{i}</b>: {request.Headers[i]} </p>");
+        }
+
+        response.Write($"<p><b>File content</b>: </p>");
+
+        using (StreamReader sr = new StreamReader(request.PhysicalApplicationPath + request.FilePath))
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                response.Write($"<p>{sr.ReadLine()} </p>");
+            }            
+        }
+    }
+}
